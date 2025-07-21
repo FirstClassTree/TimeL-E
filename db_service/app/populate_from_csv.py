c3e0c2cf497c621fa5ef244c9eec603c93fbc155
anna-pett <93157544+anna-pett@users.noreply.github.com>
2025-07-21 22:45:16 +0300

    Revert "/data at root"
    This reverts commit 58104f38d60d86d0e086b39b61eea4a6c4b50a79.

--- db_service/app/populate_from_csv.py
+++ db_service/app/populate_from_csv.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app.models import Product, Department, Aisle
 
-CSV_DIR = "/data"
+CSV_DIR = "/app/csv_data"
 
 def populate_tables():
     db: Session = SessionLocal()
